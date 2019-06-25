d431dc34e71f4d0a66788e10b0a390eee0661d38
noname21062000 <noname21062000@gmail.com>
2019-06-25 10:42:55 +0700

    remove font

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -27,22 +27,22 @@ function pc_add_button_chat(){
 
 add_action('add_button_chat', 'pc_add_button_chat',11,1);
 
-function remove_text_add_to_cart($args){
-    global $product;
-    echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-        sprintf( '<div class="box-hover hidden-sm hidden-xs hidden-md">
-                    <div class="wrap_add_cart">
-                    <a href="%s" data-product_id="%s" class="%s" %s><i class="fas fa-shopping-cart"></i></a>
-                    </div>
-                </div>',
-            esc_url( $product->add_to_cart_url() ),
-            esc_attr($product->id) ,
-            esc_attr('ajax_add_to_cart add_to_cart_button product_type_simple primary btn_add_cart' ),
-            isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-        ),
-    $product, $args );
-}
-add_action('add_button_chat', 'remove_text_add_to_cart', 12);
+// function remove_text_add_to_cart($args){
+//     global $product;
+//     echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+//         sprintf( '<div class="box-hover hidden-sm hidden-xs hidden-md">
+//                     <div class="wrap_add_cart">
+//                     <a href="%s" data-product_id="%s" class="%s" %s><i class="fas fa-shopping-cart"></i></a>
+//                     </div>
+//                 </div>',
+//             esc_url( $product->add_to_cart_url() ),
+//             esc_attr($product->id) ,
+//             esc_attr('ajax_add_to_cart add_to_cart_button product_type_simple primary btn_add_cart' ),
+//             isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+//         ),
+//     $product, $args );
+// }
+// add_action('add_button_chat', 'remove_text_add_to_cart', 12);
 
 function edit_format_current_dong($price,$product){
     if(is_single()){
