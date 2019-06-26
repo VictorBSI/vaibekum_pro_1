30bd0bb08d2600951e0d0835ad923f828c716de6
An Cao <ancao@giaonhanh.net>
2019-06-26 20:41:05 +0700

    thêm đoạn code 0đ thành liên hệ

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -140,3 +140,19 @@ function woocommerce_get_custom_availability( $data, $product ) {
     return $data;
 }
 add_action('woocommerce_get_availability', 'woocommerce_get_custom_availability', 10, 2);
+
+/* THAY ĐỔI 0đ THÀNH LIÊN HỆ*/
+
+function vbk_wc_custom_get_price_html( $price, $product ) {
+    if ( $product->get_price() == 0 ) {
+        if ( $product->is_on_sale() && $product->get_regular_price() ) {
+            $regular_price = wc_get_price_to_display( $product, array( 'qty' => 1, 'price' => $product->get_regular_price() ) );
+
+            $price = wc_format_price_range( $regular_price, __( 'Free!', 'woocommerce' ) );
+        } else {
+            $price = '<span class="amount">' . __( 'Liên hệ', 'woocommerce' ) . '</span>';
+        }
+    }
+    return $price;
+}
+add_filter( 'woocommerce_get_price_html', 'vbk_wc_custom_get_price_html', 10, 2 );
--- wp-content/themes/flatsome-child/header.php
+++ wp-content/themes/flatsome-child/header.php
@@ -12,24 +12,18 @@
 	<?php wp_head(); ?>
 </head>
 <script>
-	jQuery(document).ready(function () {
-		jQuery("#mega-menu-title").click(function () {
-
+	jQuery(document).ready(function() {
+		jQuery("#mega-menu-title").click(function() {
 			jQuery("#mega_menu").toggleClass("active")
-
 		}),
-		jQuery("body").click(function (e) {
-
+		jQuery("body").click(function(e) {
 			var i = jQuery(e.target);
 			"mega-menu-title" != i.attr("id") && jQuery("#mega_menu.active").removeClass("active")
-
 		}),
-		jQuery("#mega_menu>li").each(function (e) {
-
-			jQuery(this).children(".sub-menu").css("margin-top", 37 * -e + "px"), jQuery(this).children(".menu-image").css("margin-top", 37 * -e + "px"), jQuery(this).find("li").each(function (e) {
-				jQuery(this).children(".menu-image").css("margin-top", 36 * -e + "px")
+		jQuery("#mega_menu>li").each(function(e) {
+			jQuery(this).children(".sub-menu").css("margin-top", 37 * -e + "px"), jQuery(this).children(".menu-image").css("margin-top", 37 * -e + "px"), jQuery(this).find("li").each(function(e) {
+			jQuery(this).children(".menu-image").css("margin-top", 36 * -e + "px")
 			})
-
 		})
 	});
 </script>
