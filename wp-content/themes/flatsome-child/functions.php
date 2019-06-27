1db2a63136086efa41c35c05ac57ccc61e08b4cd
noname21062000 <noname21062000@gmail.com>
2019-06-27 10:27:15 +0700

    css button next , prev carousel

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -282,6 +282,9 @@ function flatsome_mayphotocomvn_text_strings( $translated_text, $text, $domain )
         case 'Related products' :
             $translated_text = __('Sản phẩm cùng loại', 'woocommerce');
             break;
+        case 'No products were found matching your selection.' :
+            $translated_text = __('Không có sản phẩm nào trong danh mục này.', 'woocommerce');
+            break;
     }
     return $translated_text;
 }
