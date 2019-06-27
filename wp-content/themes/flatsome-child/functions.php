bd73f2c699f83da76738a4ad696cd803120a01c8
noname21062000 <noname21062000@gmail.com>
2019-06-27 08:51:03 +0700

    fix icon main menu

--- wp-content/themes/flatsome-child/functions.php
+++ wp-content/themes/flatsome-child/functions.php
@@ -147,7 +147,7 @@ add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10,
 function change_existing_currency_symbol($currency_symbol, $currency) {
     switch ($currency) {
         case 'VND':
-            $currency_symbol = ' ₫';
+            $currency_symbol = ' đ';
             break;
     }
     return $currency_symbol;
