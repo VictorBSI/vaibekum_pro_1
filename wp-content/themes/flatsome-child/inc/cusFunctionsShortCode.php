c56a0eac685c14d8d14844e08f2e5db3e13ef733
noname21062000 <noname21062000@gmail.com>
2019-06-28 22:53:34 +0700

    create shortcode footer

--- wp-content/themes/flatsome-child/inc/cusFunctionsShortCode.php
+++ wp-content/themes/flatsome-child/inc/cusFunctionsShortCode.php
@@ -272,6 +272,7 @@ if(!function_exists('create_shortcode_show_tags')) {
     function create_shortcode_show_tags() {
         $post_tags = get_the_tags();
         if (!empty($post_tags)) {
+			$output = '';
             $output .= '<div class="title-name-tags">Tags: ';
             foreach ($post_tags as $tag) {
                 $output .= '<a href="' . get_tag_link($tag->term_id) . '">' . $tag->name . '</a>';
@@ -414,3 +415,257 @@ if (!function_exists('create_short_code_show_category_blog')) {
     add_shortcode( 'SHOW-CATEGORY', 'create_short_code_show_category_blog');
 }
 
+/** TẠO WIDGET SUPPORT ONLINE */
+
+function create_widget_ho_tro_online(){
+    $title = get_field('title', 'option');
+    $address = get_field('address' , 'option');
+    $phone_suppport = get_field('vbk_phone', 'option');
+    $time_work = get_field('vbk_time_work', 'option');
+    $html = '';
+	$html .= '<div class="support-content">';
+	$html .= '<span class="widget-title shop-sidebar">'.$title.'</span>';
+	$html .= '<div class="support_content">';
+    $html .= '<p class="sub-showroom"><a target="_blank" href="https://maps.google.com/?ll='.$address['lat'].','.$address['lng'].'"><i class="fas fa-map-marker-alt"></i>'.$address['address'].'</a></p>';
+    $html .= '<span class="color-support">Hỗ trợ:</span>';
+   
+    if(!empty( $phone_suppport )){
+        $html .= '<i class="fas fa-phone phone">  '.$phone_suppport.'</i>';
+    }
+	$html .= '<span class="color-support-time-work">Thời gian làm việc : '. $time_work.'</span>';  
+	$html .= '</div>';
+	$html .= '</div>';
+    return $html;
+}
+add_shortcode('WIDGET_SUPPORT_ONLINE', 'create_widget_ho_tro_online');
+
+/** TẠO WIDGET FOOTER TWO  */
+
+function create_footer_two(){
+
+	$location = get_field('address', 'option');
+			$html = '';
+    		$html .= '<div class="acf-map">';
+        	$html .= '<div class="marker" data-lat="'. $location['lat'].'" data-lng="'. $location['lng'] .'"></div>';
+			$html .= '</div>'
+			?>
+            <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBzB4oH-3da_7O2h67LyXCos6kjyqhGxvw"></script>   
+            <script type="text/javascript">
+                (function($) {
+
+                /*
+                *  new_map
+                *
+                *  This function will render a Google Map onto the selected jQuery element
+                *
+                *  @type	function
+                *  @date	8/11/2013
+                *  @since	4.3.0
+                *
+                *  @param	$el (jQuery element)
+                *  @return	n/a
+                */
+
+                function new_map( $el ) {
+                    
+                    // var
+                    var $markers = $el.find('.marker');
+                    
+                    
+                    // vars
+                    var args = {
+                        zoom		: 16,
+                        center		: new google.maps.LatLng(0, 0),
+                        mapTypeId	: google.maps.MapTypeId.ROADMAP
+                    };
+                    
+                    
+                    // create map	        	
+                    var map = new google.maps.Map( $el[0], args);
+                    
+                    
+                    // add a markers reference
+                    map.markers = [];
+                    
+                    
+                    // add markers
+                    $markers.each(function(){
+                        
+                        add_marker( $(this), map );
+                        
+                    });
+                    
+                    
+                    // center map
+                    center_map( map );
+                    
+                    
+                    // return
+                    return map;
+                    
+                }
+
+                /*
+                *  add_marker
+                *
+                *  This function will add a marker to the selected Google Map
+                *
+                *  @type	function
+                *  @date	8/11/2013
+                *  @since	4.3.0
+                *
+                *  @param	$marker (jQuery element)
+                *  @param	map (Google Map object)
+                *  @return	n/a
+                */
+
+                function add_marker( $marker, map ) {
+
+                    // var
+                    var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+
+                    // create marker
+                    var marker = new google.maps.Marker({
+                        position	: latlng,
+                        map			: map
+                    });
+
+                    // add to array
+                    map.markers.push( marker );
+
+                    // if marker contains HTML, add it to an infoWindow
+                    if( $marker.html() )
+                    {
+                        // create info window
+                        var infowindow = new google.maps.InfoWindow({
+                            content		: $marker.html()
+                        });
+
+                        // show info window when marker is clicked
+                        google.maps.event.addListener(marker, 'click', function() {
+
+                            infowindow.open( map, marker );
+
+                        });
+                    }
+
+                }
+
+                /*
+                *  center_map
+                *
+                *  This function will center the map, showing all markers attached to this map
+                *
+                *  @type	function
+                *  @date	8/11/2013
+                *  @since	4.3.0
+                *
+                *  @param	map (Google Map object)
+                *  @return	n/a
+                */
+
+                function center_map( map ) {
+
+                    // vars
+                    var bounds = new google.maps.LatLngBounds();
+
+                    // loop through all markers and create bounds
+                    $.each( map.markers, function( i, marker ){
+
+                        var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+                        bounds.extend( latlng );
+
+                    });
+
+                    // only 1 marker?
+                    if( map.markers.length == 1 )
+                    {
+                        // set center of map
+                        map.setCenter( bounds.getCenter() );
+                        map.setZoom( 16 );
+                    }
+                    else
+                    {
+                        // fit to bounds
+                        map.fitBounds( bounds );
+                    }
+
+                }
+
+                /*
+                *  document ready
+                *
+                *  This function will render each map when the document is ready (page has loaded)
+                *
+                *  @type	function
+                *  @date	8/11/2013
+                *  @since	5.0.0
+                *
+                *  @param	n/a
+                *  @return	n/a
+                */
+                // global var
+                var map = null;
+
+                $(document).ready(function(){
+
+                    $('.acf-map').each(function(){
+
+                        // create map
+                        map = new_map( $(this) );
+
+                    });
+
+                });
+
+            })(jQuery);
+			</script>
+		<?php
+    return $html;
+}
+add_shortcode('WIDGET_FOOTER_TWO', 'create_footer_two');
+
+/** FOOTER COLLUMN ONE */
+
+
+/* CREATE SHORT CODE SHOW CATEGORY BLOG */
+if (!function_exists('footer_column_one')) {
+    function footer_column_one() {
+		$logo = get_field('vbk_logo_footer', 'option');
+		$address = get_field('vbk_address', 'option');
+		$mail = get_field('vbk_email_hotline', 'option');
+		$name = get_bloginfo('home');
+		$str = preg_replace('#^https?://#', '', $name);
+		$shopee = get_field('vbk_shopee', 'option');
+		$lazada = get_field('vbk_lazada', 'option');
+		$sendo = get_field('vbk_sendo', 'option');
+		$xhtml = '';
+		if(!empty($logo['url'])){
+        $xhtml.= '<div class="ux-logo has-hover align-middle ux_logo inline-block has-block tooltipstered" style="max-width: 100%!important; width: 330px!important">
+						<div class="ux-logo-link block image-" title="'.$logo['alt'].'" target="_self" href="" style="padding: 15px;"><img src="'.$logo['url'].'" title="'.get_bloginfo('name').'" alt="'.get_bloginfo('name').'" class="ux-logo-image block" style="height:49px;"></div>
+					</div>';
+		}
+		$xhtml .= '<div class="footer-content">
+						<ul class="info">';
+						if(!empty($address)){
+							$xhtml .= '<li>'.$address.'</li>';
+						}
+							$xhtml .= '<li>Website: <a href="/">'.$str.'</a></li>';
+						if(!empty($mail)){
+							$xhtml .= '<li>Email: <a target="_blank" href="mailto:'.$mail.'">'.$mail.'</a></li>';
+						}	
+						if(!empty($shopee)){
+							$xhtml .= '<li>Shopee: <a  target="_blank" href="'.$shopee.'">'.$shopee.'</a></li>';
+						}	
+						if(!empty($lazada)){
+							$xhtml .= '<li>Lazada: <a target="_blank" href="'.$lazada.'">'.$lazada.'</a></li>';
+						}	
+						if(!empty($sendo)){
+							$xhtml .= '<li>Sendo: <a target="_blank" href="'.$sendo.'">'.$sendo.'</a></li>';
+						}	
+		$xhtml .='</ul></div>';
+        return $xhtml;
+    }
+    add_shortcode( 'SHOW-FOOTER-ONE', 'footer_column_one');
+}
